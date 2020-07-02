f381e2ed1a11dcd252a2eb08b67b0a7c1a378b14
LiuM0805 <719998141@qq.com>
2020-07-02 13:36:57 +0800

    update:集合用法

--- demo.py
+++ demo.py
@@ -1,16 +1,25 @@
-# r 原样输出字符
-print("C:\\User\name")
+# 字符
+print("Hello World")
+# r：原样输出字符
 print(r'C:\User\name')
-
 # f和format两种：字符内添加变量
 x = 'abc'
 print(f'C:\\User\\name\\{x}')
 print("C:\\User\\name\\{a}\\{b}".format(b=123, a='abc'))
 
-# 列表及切片打印
+# 列表
 Mio_list = [1, 'abc', 7, 15, "python"]
 print(Mio_list[1:3])
-
 # 字符列表
 python = "hello"
 print(python[0])
+
+# 集合
+basket1 = {"apple", "banana", "a"}
+basket2 = {"pear", "orange", "a"}
+# 合并两个集合
+print(basket1.union(basket2))
+# 打印相同集合
+print(basket1.intersection(basket2))
+# 打印不同集合
+print(basket1.difference(basket2))
