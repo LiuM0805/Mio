cd169df7befcac7b374b8cf02dd38972c07f8afd
LiuM0805 <719998141@qq.com>
2020-07-03 22:24:28 +0800

    update:Pytest创建类管理多个case

--- Py_test/test_demo.py
+++ Py_test/test_demo.py
@@ -6,3 +6,16 @@ def inc(x):
 # 修改IDE默认使用Pytest执行case，就会有绿箭头了
 def test_answer():
     assert inc(3) == 5
+
+
+# Pytest的类无需继承unittest
+class TestClass:
+    name = 'xiaomi'
+
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
