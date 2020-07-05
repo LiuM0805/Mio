59d74f5d3f146ceba681914e8a9050a4cb0890c4
LiuM0805 <719998141@qq.com>
2020-07-05 22:02:04 +0800

    update:Pytest的测试装置，执行先后顺序

--- Py_test/test_demo.py
+++ Py_test/test_demo.py
@@ -8,8 +8,26 @@ def test_answer():
     assert inc(3) == 5
 
 
+# Pytest的测试装置 - module最先执行
+def setup_module():
+    print("setup module")
+
+# Pytest的测试装置 - function第二执行
+def setup_function():
+    print("setup function")
+
+
 # Pytest的类无需继承unittest
 class TestClass:
+    # Pytest的测试装置 - setup最后执行
+    def setup(self):
+        print("setup")
+
+    # 给类定义方法加@classmethod，第三执行
+    @classmethod
+    def setup_class(cls):
+        print("setup class")
+
     name = 'xiaomi'
 
     def test_one(self):
