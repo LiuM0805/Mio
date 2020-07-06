746acb9f2047f04b1e3c26297d788bd9d9bd59d7
LiuM0805 <719998141@qq.com>
2020-07-06 13:17:46 +0800

    update:Pytest标签分组使用

--- Unit_test/test_div.py
+++ Unit_test/test_div.py
@@ -1,5 +1,7 @@
 # 导入div函数
 from Unit_test.div import div
+# pytest标签需导入
+import pytest
 
 
 # 作业：测试div函数
@@ -7,6 +9,8 @@ def test_div_int():
     assert div(10, 5) == 2
 
 
+# Pytest标签，也叫装饰器，执行它：pytest -m "tab"
+@pytest.mark.tab
 def test_div_float():
     assert div(6.5, 3.2) == 2.03125
 
